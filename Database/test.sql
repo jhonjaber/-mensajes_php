41cec938d06bb1a63cb0314abdf2f5ff47fc803d
jhonjaber <jgiraldo@uc.edu.co>
2020-11-03 21:24:59 -0500

    Se borran datos sensibles

--- Database/test.sql
+++ Database/test.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2020 a las 01:29:27
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.34
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `test`
---
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `mensajes`
---
 
 CREATE TABLE `mensajes` (
   `id_mensaje` int(7) NOT NULL,
@@ -34,11 +11,7 @@ CREATE TABLE `mensajes` (
   `id_usuario` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(2) NOT NULL,
@@ -48,13 +21,8 @@ CREATE TABLE `usuarios` (
   `clave` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `mensajes`
---
+
 ALTER TABLE `mensajes`
   ADD PRIMARY KEY (`id_mensaje`);
 
@@ -89,8 +57,5 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `mensajes` (`id_mensaje`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
