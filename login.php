fb7015b013118e9f7fa73d502fa7b2bb1997709d
jhonjaber <jgiraldo@uc.edu.co>
2020-11-05 19:08:13 -0500

    los cambios

--- login.php
+++ login.php
@@ -2,49 +2,31 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Registro de usuario - mensajes App</title>
+		<title>Inicio de sesión - mensajes App</title>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<header>
 			<h1>mensajes App</h1>
-			<h2>Registro de usuario</h2>
+			<h2>Inicio de sesión</h2>
 		</header>
 		<section>
 			<div class="row">
 				<div class="col-md-3"></div>
 				<div class="col-md-6">
 					<div>
-						<p>Si tienes una cuenta creada, <a href="login.php">inicia sesión</a> </p>
+						<p>Si tienes una cuenta creada, inicia sesión, de lo contrario, puedes <a href="registro.php">registrarte</a> </p>
 					</div>
 					<form name="login" action="" method="POST">
 					  	<div class="form-group">
-						    <label for="nombre_completo">Nombre completo</label>
-						    <input type="text" class="form-control" id="nombre_completo" name="nombre_completo" aria-describedby="nombre-ayuda">
-					    	<small id="nombre-ayuda" class="form-text text-muted">Tu nombre completo .</small>
+						    <label for="exampleInputEmail1">Usuario</label>
+						    <input type="email" class="form-control" id="usuario" name="usuario" aria-describedby="emailHelp">
+					    	<small id="emailHelp" class="form-text text-muted">No compartiremos tu información personal.</small>
 					  	</div>
-
 					  	<div class="form-group">
-						    <label for="correo">Correo</label>
-						    <input type="email" class="form-control" id="correo" name="correo" aria-describedby="correo-ayuda">
-					    	<small id="correo-ayuda" class="form-text text-muted">No te enviaremos Spam, allí podrás recuperar tu cuenta.</small>
-					  	</div>
-
-					  	<div class="form-group">
-						    <label for="celular">Celular</label>
-						    <input type="tel" class="form-control" id="celular" name="celular" aria-describedby="celular-ayuda">
-					    	<small id="celular-ayuda" class="form-text text-muted">también puedes usar tu número para el login.</small>
-					  	</div>
-
-					  	<div class="form-group">
-					    	<label for="clave">Contraseña</label>
+					    	<label for="exampleInputPassword1">Contraseña</label>
 					    	<input type="password" class="form-control" id="clave" name="clave">
-					  	</div>
-
-					  	<div class="form-group">
-					    	<label for="clave2">Verifica tu Contraseña</label>
-					    	<input type="password" class="form-control" id="clave2" name="clave2">
 					  	</div>
 					  	<button type="submit" name="enviar" class="btn btn-primary">Enviar</button>
 					</form>
